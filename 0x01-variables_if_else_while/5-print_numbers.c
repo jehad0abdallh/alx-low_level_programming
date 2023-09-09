a42d72e39a256f9dff099a2273fc2a498303f85b
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-09 14:36:39 -0700

    0x01. C - Variables, if, else, while answar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - main block 
+ * Description :print all single digit numbers of base 10
+ * start from 0 , followed by new line .
+ * return :0
+ * */
+int main(void)
+{
+	char ch = '0';
+
+	while (c <= '9')
+	{
+		putchar(ch);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
