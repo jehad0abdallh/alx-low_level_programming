dc772dd68e8c37a2a2327a3a122fbfba6c11f986
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-09 14:18:47 -0700

    0x01. C - Variables, if, else, while answar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:  prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	ch= 'A';
+
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
