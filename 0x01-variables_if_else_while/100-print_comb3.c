6a67ad6e0d68d1941a591680f8c36dc22ce2a620
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-09 15:16:59 -0700

    0x01. C - Variables, if, else, while answar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: Write a program that prints all possible different combinations of two digits.
+ * Description: prints all possible different combinations of two digits.
  * Numbers must be separated by ,, followed by a space
  * The two digits must be different
  * 01 and 10 are considered the same combination of the two digits 0 and 1
