195f06c45a35f0de237204289cd5f7c40454464c
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-12 09:19:51 -0700

    0x02. C - Functions, nested loops answar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
 */
 void print_alphabet_x10(void)
 {
-        int i  = 0 ;
-        char letter ;
+        int i  = 0;
+        char letter;
 
-        while( i++ <= 9)
+        while (i++ <= 9)
         {
                 for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter) ;
+                _putchar(letter);
 
-                _putchar(10) ;
+                _putchar(10);
         }
 }
