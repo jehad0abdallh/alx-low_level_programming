e8ce87c66cc974b9280eaa7779e8b9df050abe8c
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-12 10:14:11 -0700

    0x02. C - Functions, nested loops answar

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _abs - prints the absolute value of a number
+*@i: number targeted
+*Return: returns 0
+*/
+int print_last_digit(int i)
+{
+int re;
+re = i % 10;
+return re;
+}
