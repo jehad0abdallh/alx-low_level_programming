bee5a4814d2b937b01692646259d4fffc0219fe3
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-14 13:06:06 -0700

    0x04-more_functions_nested_loops alx

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_diagonal - printing lines diagonal
- * @n: input variable
+ * print_square - prints a square of size size
+ * @size: size of the square
  *
  * Return: void
  */
@@ -9,9 +9,12 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size <= 0)
+		_putchar('\n');
+
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < (size); j++)
 		{
 			_putchar('#');
 		}
