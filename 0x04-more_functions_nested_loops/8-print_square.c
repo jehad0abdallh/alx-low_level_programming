7a72670a9ff5aa1eaa4ea98f1d7b200cc18dcd88
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-14 13:03:41 -0700

    0x04-more_functions_nested_loops alx

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagonal - printing lines diagonal
+ * @n: input variable
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
