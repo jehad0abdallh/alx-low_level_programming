627592a8128c5aa1288bbbddbed498bf3654c1bf
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-18 11:05:51 -0700

    0x05-pointers_arrays_strings answar

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_rev - print revrs string
+ * @s: first input
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
