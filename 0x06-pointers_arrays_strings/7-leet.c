a704f0f646508b9b023b5bfabdd936279f788bc0
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-21 14:00:37 -0700

    0x06-pointers_arrays_strings answar

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,17 +7,18 @@
 
 char *leet(char *str)
 {
+	char *cp = str;
 	char symb[] = {'a', 'e', 'o', 't', 'i'};
 	int rep[] = {4, 3, 0, 7, 1};
 	unsigned int i, j;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str)
 	{
-	for (j = 0; symb[j] != '\0'; j++)
-	{
-		if (str[i] == symb[j] || str[i] == symb[j] - 32)
-			str[i] = rep[j];
-	}
+		for (j = 0; j < sizeof(symb) / sizeof(char); j++)
+		{
+			if (*str == symb[j] || *str == symb[j] - 32)
+				*str = 48 + rep[j]
+		}
 	}
 	return (str);
 }
