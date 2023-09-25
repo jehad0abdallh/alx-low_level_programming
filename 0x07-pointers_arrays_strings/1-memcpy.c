982a9c6e1d98bc8ab1b210b974feb5de8f970b22
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-25 06:24:32 -0700

    0x07-pointers_arrays_strings answar

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ *_memcpy -  replace part of sting by string
+ *@dest: string pointer.
+ *src: constent sting.
+ *n: string lingth.
+ *Return: pointer to char.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
