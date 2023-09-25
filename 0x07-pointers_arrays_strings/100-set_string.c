a24f7ea0d6c29c8cecf19b868b5dd12b295d233b
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-25 10:23:19 -0700

    0x07-pointers_arrays_strings answar

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ *set_string - function that searches a string for any of a set of bytes.
+ *@s: pointer of pointer char.
+ *@to: pointer
+ *Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
