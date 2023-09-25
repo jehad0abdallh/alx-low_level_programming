fcfcfd2e667c54d17a8700ffddb5fb720cf8d6c5
jehad0abdallh <gehad0elshi8@gmail.com>
2023-09-25 07:34:20 -0700

    0x07-pointers_arrays_strings answar

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ *_strbrk - function that searches a string for any of a set of bytes.
+ *@s: string pointer.
+ *@accept: function that gets the length of a prefix substring.
+ *Return: pointer for string.
+ */
+
+char *_strpbrk(char *s, char *accept);
+{
+	unsigned int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
