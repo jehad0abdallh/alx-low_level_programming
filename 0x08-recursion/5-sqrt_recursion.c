81cfba772be3831d150b6a267aeff213c3ad117a
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-02 01:17:56 +0300

    0x08-recursion in C answar

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * sqrt - root sqrt
+ * sqrtt- root sqrt
  * @a: same number as n
  * @b: int
  * Return: On success 1.
  */
-int sqrt(int a, int b)
+int sqrtt(int a, int b)
 {
 	if (b * b == a)
 		return (b);
 	else if (b * b < a)
-		return (sqrt(a, b + 1));
+		return (sqrtt(a, b + 1));
 	else
 		return (-1);
 	
@@ -22,5 +22,5 @@ int sqrt(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (sqrtt(n, 1));
 }
