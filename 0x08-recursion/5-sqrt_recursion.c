906e407420e5a762b455d31d65a4e591a89c4f6b
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-02 01:10:29 +0300

    0x08-recursion in C answar

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,8 @@ int sqrt(int a, int b)
 		return (b);
 	else if (b * b > a)
 		return (-1);
-	return (sqrt(a, b + 1));
+	else
+		return (sqrt(a, b + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of n
