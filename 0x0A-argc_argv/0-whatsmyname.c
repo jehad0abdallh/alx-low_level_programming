3e4379ccfdfb95c2dbab57aaf4f4274939f0a37e
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-02 14:45:53 -0700

    0x0A-argc_argv in C answar

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - block main print programe name
+ * @argc: int
+ * @argv: array of char of program token
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
