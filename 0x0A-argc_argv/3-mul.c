424d720f65640a4293c46f998522826efa8409dc
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-02 14:55:11 -0700

    0x0A-argc_argv in C answar

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - block main print programe name
+ * @argc: int
+ * @argv: array of char of program token
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
