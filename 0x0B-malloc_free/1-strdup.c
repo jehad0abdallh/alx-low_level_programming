90af841117b8ea85d049f20e4ce72c440ea3197a
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-04 14:18:38 -0700

    0x0B-malloc_free in C answar

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strdup -  pointer to a newly allocated space in memory
+ * @str: pointer of arrary
+ * Return: new coby of string allocated in memory
+ */
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *arr;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size] != '\0'; size++)
+		;
+	arr = malloc(size * sizeof(*str) + 1);
+
+	if (arr == 0)
+		return (NULL);
+	else
+	{
+		for (; i < size; i++)
+			arr[i] = str[i];
+	}
+	return (arr);
+}
