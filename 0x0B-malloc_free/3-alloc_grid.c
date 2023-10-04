204c0619381e4b826164820fce8961b835e708f3
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-05 01:48:01 +0300

    0x0B-malloc_free in C answar

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - finction for two dimantinal arrey
+ * @width: int
+ * @height: int
+ * Return: pointer of pointer of int 
+ */
+int **alloc_grid(int width, int height)
+{
+	int **tab, i, j;
+	if (width <= 0 || height <= 0)
+	{
+		return  (NULL);
+	}
+	tab = malloc(height * sizeof(int *));
+	if (tab == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		tab[i] = malloc(width * sizeof(int));
+		if (tab[i] == NULL)
+		{
+			for (j = 0; j < i;  j++)
+				free(tab[j]);
+			free(tab);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			tab[i][j] = 0;
+		}
+	}
+	return (tab);
+}
