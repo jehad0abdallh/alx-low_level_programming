9d0f99153484c1c5eac6005cdd39fb2dfe521b75
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-05 01:58:32 +0300

    0x0B-malloc_free in C answar

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * free_grid - frees a 2 dimensional grid previously created by your alloc_grid
+ * @grid: grid in memory for pointer of pointer
+ * @height: int of size
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
