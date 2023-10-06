7238a8a015a5be84d7aa13c35f1fcf457f193b3f
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-06 03:53:05 +0300

    0x0C-more_malloc_free in C answar

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_range - Create array of integers filled with numbers from min to max
+ * @min: First number in the array
+ * @max: Largest number in the array
+ *
+ * Return: Pointer to new array, NULL if failed
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, size, n;
+	/*for valid error*/
+	if (min > max)
+		return (NULL);
+	size = max - min;
+	arr = malloc((size + 1) * sizeof(int));
+	i = 0;
+	n = min;
+	while (n <= max)
+	{
+		arr[i] = n;
+		i++;
+		n++;
+	}
+	return (arr);
+}
