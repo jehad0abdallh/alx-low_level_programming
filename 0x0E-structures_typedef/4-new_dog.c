b93c6334b2b20d30381fb5299934cea6af48417b
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-10 20:41:13 +0300

    0x0E-structures_typedef in C answar

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include"dog.h"
+#include<stdlib.h>
+/**
+ * new_dog = creat struct with typedef
+ * @name: pointer
+ * @age: flout
+ * @owner: pointr
+ * Return: struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+	int i, ln, lo;
+
+	new_d = malloc(sizeof(*new_d));
+	if (new_d == NULL || !(name) || !(owner))
+	{
+		free(new_d);
+		return (NULL);
+	}
+	else
+	{
+		new_d->name = name;
+		new-d->age = age;
+		new_d->owner = owner;
+	}
+	return (new_d);
+}
