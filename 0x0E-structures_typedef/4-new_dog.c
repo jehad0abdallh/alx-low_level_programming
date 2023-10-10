6f37d661bafdffeb79c43a2cfd74a8fd6b73dadd
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-10 21:12:49 +0300

    0x0E-structures_typedef in C answar

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,11 +18,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_d);
 		return (NULL);
 	}
-	else
+	for (ln = 0; name[ln]; ln++)
+		;
+	for (lo = 0; owner[lo]; lo++)
+		;
+
+	new_d->name = malloc(ln + 1);
+	new_d->owner = malloc(lo + 1);
+
+	if (!(new_d->name) || !(new_d->owner))
 	{
-		new_d->name = name;
-		new_d->age = age;
-		new_d->owner = owner;
+		free(new_d->name);
+		free(new_d->owner);
+		free(new_d);
+		return (NULL);
 	}
+	for (i = 0; i < ln; i++)
+		new_d->name[i] = name[i];
+	new_d->name[i] = '\0';
+	new_d->age = age;
+
+	for (i = 0; i < lo; i++)
+		new_d->owner[i] = owner[i];
+	new_d->owner[i] = '\0';
+
 	return (new_d);
 }
