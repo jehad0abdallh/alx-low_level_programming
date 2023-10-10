2435a12939f76e2c07fda9a1ceb83ce95f84d767
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-10 20:03:46 +0300

    0x0E-structures_typedef in C answar

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,25 +8,19 @@
  **/
 void print_dog(struct dog *d)
 {
-
-	if (d == NULL)
-	{
-		printf("");
-		return (0);
-	}
-	else
+	if (d)
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
-			printf("Name: %s \n", d->name);
+			printf("Name: %s\n", d->name);
 		if (d->age <= 0)
-			printf("age: (nil)\n");
+			printf("Age: (nil)\n");
 		else
-			printf("age: %f \n", d->age);
+			printf("Age: %f \n", d->age);
 		if (d->owner == NULL)
-			printf("owner: (nil)\n");
+			printf("Owner: (nil)\n");
 		else
-			printf("owner: %s \n", d->owner);
+			printf("Owner: %s \n", d->owner);
 	}
 }
