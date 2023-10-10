ebfdab4850c580c44f20ec7a4d84170b1562d689
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-10 16:16:13 +0300

    0x0E-structures_typedef in C answar

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - define dog
+ * @name: char
+ * @age: flout
+ * @owner: char
+ * Description: for dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
