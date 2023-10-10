39714912fb8e1a340e0f03705fdfb6169a2d36c0
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-10 21:21:00 +0300

    0x0E-structures_typedef in C answar

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"dog.h"
+/*
+ * free_dog - delelt dog struct
+ * @d: stxaruct pointer
+ *Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d);
+	}
+}
