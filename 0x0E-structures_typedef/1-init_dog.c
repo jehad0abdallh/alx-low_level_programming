bca61f638181baeb07222fa0837b7e900dce6853
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-10 16:27:04 +0300

    0x0E-structures_typedef in C answar

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/*
+ * init_dog - function to initale to dog structure
+ * @d: struct for dog
+ * @name: char pointer
+ * @age: flout
+ * @owner: char pointer
+ * Return: void
+ * */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = onwer;
+	}
+}
