9070de352bba367f569afb73e11661be647f5d2d
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-11 23:13:31 +0300

    0x0F-function_pointers C

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include"function_pointers.h"
+/**
+ * array_iterator - call function for array
+ * @array: pointer int
+ * @size: array size in size_t way to hande 32&64
+ * @action: function pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *end = array + size - 1;
+
+	if (array && size && action)
+	{
+		while (array <= end)
+			action(*array++);
+	}
+}
