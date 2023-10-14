8017b36470c01ef6211e915fd00b533356fe378a
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-14 14:23:20 +0300

    updatas C

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,8 @@
 #include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+#include<stdlib.h>
+
 /**
  * sum_them_all - sum of all its parameters
  * @n: total number of parameters
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS H
 #define VERIADIC_FUNCTIONS_H
-#include <stdlib.h>
-#include <stdio.h>
+
+
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
