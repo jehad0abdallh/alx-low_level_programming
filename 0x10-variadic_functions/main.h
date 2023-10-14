44864d8ae8a277278a280aaa8244beb69557bfb1
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-14 12:20:03 +0300

    0x10-variadic_functions C

---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
