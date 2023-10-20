a978427ab2d2e62e6f91fe9c65c1de651e2ec9f4
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-20 11:54:51 +0300

    0x0D-preprocessor in C answar

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * struct list_t -single linced list for string
+ * struct list_n -single linced list for string
  * @str: char pointer
  * @len: string lin
  * @next: pointer for next node
