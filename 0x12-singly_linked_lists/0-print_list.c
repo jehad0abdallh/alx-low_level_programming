4bfa255a1453cab09a4fd1ca635db9a161153d4b
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-20 04:46:58 +0300

    linkedlist in C answar

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,15 +1,18 @@
 #include"lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * print_list - print linked list element
  * @h: list_t struct
- * Resturn: size_t of listcount
+ * Return: size_t of listcount
  */
 size_t print_list(const list_t *h)
 {
 	unsigned int count = 0;
 	list_t *new;
 
-	for(; h != NULL; count++)
+	for (; h != NULL; count++)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s \n", 0, "(nil)");
@@ -19,4 +22,3 @@ size_t print_list(const list_t *h)
 	}
 	return (count);
 }
-	
