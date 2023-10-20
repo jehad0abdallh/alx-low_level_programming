87246e5a4377344cd37d93e256427db418705463
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-20 14:15:45 +0300

    linkedlist in C answar

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include"lists.h"
+/**
+ * before_main -executed befor main
+ * Return: void
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
