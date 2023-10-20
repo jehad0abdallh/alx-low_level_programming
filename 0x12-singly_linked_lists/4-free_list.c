89c2dacfc8ba6bc208c2de1f5320d2389c6a6836
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-20 13:49:36 +0300

    linkedlist in C answar

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include"lists.h"
+/**
+ * free_list - function free list_t list
+ * @head: head pointer
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}							
