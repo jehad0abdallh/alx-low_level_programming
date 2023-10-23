a325643e9e57d788d120470402e23eb858dfd860
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-24 01:14:00 +0300

    linkedlist in C answar

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,9 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	for (int i = 0; i < index && head != NULL; i++)
+	unsigned int i;
+
+	for (i = 0; i < index && head != NULL; i++)
 		head = head->next;
 	return (head);
 }
