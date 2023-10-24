4a78f50293ed9331bf1ac52f8349851d068b6443
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-24 19:00:36 +0300

    linkedlist in C answar

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include"lists.h"
+/**
+ * delete_nodeint_at_index - function to add node
+ * @head: head ponter
+ * @index: delelt node
+  * Return: int  value
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *last, *cur;
+
+	last = *head;
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && last != NULL; i++)
+			last = last->nex;
+	}
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
+		return (-1);
+	}
+	cur = last->next;
+	if (index != 0)
+	{
+		last->next = cur->next;
+		free(cur);
+	}
+	else
+	{
+		free(last);
+		*head = cur;
+	}
+	return (1);
+}
