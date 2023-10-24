b14a48d7bd9d76650c03ee29b2592fb0615ae4fd
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-24 18:34:33 +0300

    linkedlist in C answar

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include"lists.h"
+/**
+ * insert_nodeint_at_index - function to add node in index
+ * @head: head ponter
+ * @idx: index
+ * @n: new value
+ * Return: new node address or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	int i;
+	listint_t *cur, *last;
+
+	cur = malloc(sizeof(listint_t));
+	if (cur == NULL)
+		return (NULL);
+
+	last = *head;
+	if (idx != 0)
+	{
+		i = 0;
+		while (i < idx - 1 && last != NULL)
+		{
+			last = last->next;
+			i++;
+		}
+	}
+	if (last == NULL && idx != 0)
+		return (NULL);
+	cur->n = n;
+	if (idx == 0)
+	{
+		cur->next = *head;
+		*head = cur;
+	}
+	else
+	{
+		cur->next = last->next;
+		last->next = cur;
+	}
+	return (cur);
+}
+
