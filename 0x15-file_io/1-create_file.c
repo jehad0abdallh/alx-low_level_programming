c661a410bdd0be924d247ebc4294204edb64dfc2
jehad0abdallh <gehad0elshi8@gmail.com>
2023-10-31 00:29:34 +0200

    update add new task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 /*
  * create_file - to create file.
  * @filename: file name string
@@ -32,4 +32,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
